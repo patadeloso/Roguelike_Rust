9cf2866f9c774a34da914859c24f2b0a59804c0f
Shaun McManus <shaun.mcmanus@gmail.com>
2020-11-29 22:17:32 -0800

    Completed Ch5

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,3 @@
-#![warn(non_snake_case)]
-#![allow(unused_variables)]
-
-// For Capter 3
-//use rltk::{GameState, Rltk, RGB, VirtualKeyCode};
-//use specs::prelude::*;
-//use specs_derive::*;
-//use std::cmp::{min, max};
-
-// For Chapter 4
 use rltk::{GameState, Rltk, RGB};
 use specs::prelude::*;
 
@@ -19,6 +9,8 @@ mod player;
 use player::*;
 mod rect;
 pub use rect::Rect;
+mod visibility_system;
+use visibility_system::VisibilitySystem;
 
 pub struct State {
     ecs: World
@@ -26,6 +18,8 @@ pub struct State {
 
 impl State {
     fn run_systems(&mut self) {
+        let mut vis = VisibilitySystem{};
+        vis.run_now(&self.ecs);
         self.ecs.maintain();
     }
 }
@@ -38,8 +32,7 @@ impl GameState for State {
     player_input(self, ctx);
     self.run_systems();
 
-    let map = self.ecs.fetch::<Vec<TileType>>();
-    draw_map(&map, ctx);
+    draw_map(&self.ecs, ctx);
 
     let positions = self.ecs.read_storage::<Position>();
     let renderables = self.ecs.read_storage::<Renderable>();
@@ -66,10 +59,11 @@ fn main() -> rltk::BError {
   gs.ecs.register::<Position>();
   gs.ecs.register::<Renderable>();
   gs.ecs.register::<Player>();
+  gs.ecs.register::<Viewshed>();
 
-  let (rooms, map) = new_map_rooms_and_corridors();
+  let map : Map = Map::new_map_rooms_and_corridors();
+  let (player_x, player_y) = map.rooms[0].center();
   gs.ecs.insert(map);
-  let (player_x, player_y) = rooms[0].center();
 
 
   gs.ecs
@@ -81,6 +75,7 @@ fn main() -> rltk::BError {
           bg: RGB::named(rltk::BLACK),
       })
       .with(Player{})
+      .with(Viewshed{ visible_tiles : Vec::new(), range: 8, dirty: true })
       .build();
 
 
