a5d5f3e4c960489d1a12be7cd22cfba0f7c4596a
Shaun McManus <shaun.mcmanus@gmail.com>
2020-12-07 00:06:54 -0800

    Finished Ch7

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,18 @@ mod visibility_system;
 use visibility_system::VisibilitySystem;
 mod monster_ai_system;
 use monster_ai_system::MonsterAI;
+mod map_indexing_system;
+use map_indexing_system::MapIndexingSystem;
+mod melee_combat_system;
+use melee_combat_system::MeleeCombatSystem;
+mod damage_system;
+use damage_system::DamageSystem;
+
+#[derive(PartialEq, Copy, Clone)]
+pub enum RunState { AwaitingInput, PreRun, PlayerTurn, MonsterTurn }
 
 pub struct State {
     pub ecs: World,
-    pub runstate : RunState
 }
 
 impl State {
@@ -25,6 +33,12 @@ impl State {
         vis.run_now(&self.ecs);
         let mut mob = MonsterAI{};
         mob.run_now(&self.ecs);
+        let mut mapindex = MapIndexingSystem{};
+        mapindex.run_now(&self.ecs);
+        let mut melee = MeleeCombatSystem{};
+        melee.run_now(&self.ecs);
+        let mut damage_system = DamageSystem{};
+        damage_system.run_now(&self.ecs);
         self.ecs.maintain();
     }
 }
@@ -34,13 +48,35 @@ impl GameState for State {
   fn tick(&mut self, ctx : &mut Rltk) {
     ctx.cls();
 
-    if self.runstate == RunState::Running {
-        self.run_systems();
-        self.runstate = RunState::Paused;
-    } else {
-        self.runstate = player_input(self, ctx);
+    let mut newrunstate;
+    {
+        let runstate = self.ecs.fetch::<RunState>();
+        newrunstate = *runstate;
     }
 
+    match newrunstate {
+        RunState::PreRun => {
+            self.run_systems();
+            newrunstate = RunState::AwaitingInput;
+        }
+        RunState::AwaitingInput => {
+            newrunstate = player_input(self, ctx);
+        }
+        RunState::PlayerTurn => {
+            self.run_systems();
+            newrunstate = RunState::MonsterTurn;
+        }
+        RunState::MonsterTurn => {
+            self.run_systems();
+            newrunstate = RunState::AwaitingInput;
+        }
+    }
+
+    {
+        let mut runwriter = self.ecs.write_resource::<RunState>();
+        *runwriter = newrunstate;
+    }
+    damage_system::deletre_the_dead(&mut self.ecs);
 
     draw_map(&self.ecs, ctx);
 
@@ -55,9 +91,6 @@ impl GameState for State {
     }
 }
 
-#[derive(PartialEq, Copy, Clone)]
-pub enum RunState { Paused, Running }
-
 
 fn main() -> rltk::BError {
   use rltk::RltkBuilder;
@@ -66,9 +99,7 @@ fn main() -> rltk::BError {
     .build()?;
   let mut gs = State{
       ecs: World::new(),
-      runstate : RunState::Running
   };
-
 
   gs.ecs.register::<Position>();
   gs.ecs.register::<Renderable>();
@@ -76,12 +107,16 @@ fn main() -> rltk::BError {
   gs.ecs.register::<Viewshed>();
   gs.ecs.register::<Monster>();
   gs.ecs.register::<Name>();
+  gs.ecs.register::<BlocksTile>();
+  gs.ecs.register::<CombatStats>();
+  gs.ecs.register::<WantsToMelee>();
+  gs.ecs.register::<SufferDamage>();
 
   let map : Map = Map::new_map_rooms_and_corridors();
   let (player_x, player_y) = map.rooms[0].center();
 
 
-  gs.ecs
+  let player_entity = gs.ecs
       .create_entity()
       .with(Position { x: player_x, y: player_y})
       .with(Renderable {
@@ -92,6 +127,7 @@ fn main() -> rltk::BError {
       .with(Player{})
       .with(Viewshed{ visible_tiles : Vec::new(), range: 8, dirty: true })
       .with(Name{ name: "Player".to_string() })
+      .with(CombatStats{ max_hp: 30, hp: 30, defense: 2, power: 5 })
       .build();
 
 let mut rng = rltk::RandomNumberGenerator::new();
@@ -115,11 +151,15 @@ for (i,room) in map.rooms.iter().skip(1).enumerate() {
         .with(Viewshed{ visible_tiles : Vec::new(), range: 8, dirty: true })
         .with(Monster{})
         .with(Name{ name: format!("{} #{}", &name, i) })
+        .with(BlocksTile{})
+        .with(CombatStats{ max_hp: 16, hp: 16, defense: 1, power: 4 })
         .build();
 }
 
 gs.ecs.insert(map);
 gs.ecs.insert(Point::new(player_x, player_y));
+gs.ecs.insert(player_entity);
+gs.ecs.insert(RunState::PreRun);
 
   rltk::main_loop(context, gs)
 }
--- src/map.rs
+++ src/map.rs
@@ -3,22 +3,28 @@ use super::{Rect};
 use std::cmp::{max, min};
 use specs::prelude::*;
 
+const MAPWIDTH : usize = 80;
+const MAPHEIGHT : usize = 50;
+const MAPCOUNT : usize = MAPHEIGHT * MAPWIDTH;
+
 #[derive(PartialEq, Copy, Clone)]
 pub enum TileType {
     Wall, Floor
 }
 
+#[derive(Default)]
 pub struct Map {
     pub tiles : Vec<TileType>,
     pub rooms : Vec<Rect>,
     pub width : i32,
     pub height : i32,
     pub revealed_tiles : Vec<bool>,
-    pub visible_tiles : Vec<bool>
+    pub visible_tiles : Vec<bool>,
+    pub blocked : Vec<bool>,
+    pub tile_content : Vec<Vec<Entity>>
 }
 
 impl Map {
-
     pub fn xy_idx(&self, x: i32, y: i32) -> usize {
         (y as usize * self.width as usize) + x as usize
     }
@@ -50,36 +56,55 @@ impl Map {
         }
     }
 
+    fn is_exit_valid(&self, x:i32, y:i32) -> bool {
+        if x < 1 || x > self.width-1 || y < 1 || y > self.height-1 { return false; }
+        let idx = self.xy_idx(x, y);
+        !self.blocked[idx]
+    }
 
-
-pub fn new_map_rooms_and_corridors() -> Map {
-    let mut map = Map{
-        tiles : vec![TileType::Wall; 80*50],
-        rooms : Vec::new(),
-        width : 80,
-        height : 50,
-        revealed_tiles : vec![false; 80*50],
-        visible_tiles : vec![false; 80*50]
-    };
-
-    const MAX_ROOMS : i32 = 30;
-    const MIN_SIZE : i32 = 6;
-    const MAX_SIZE : i32 = 10;
-
-    let mut rng = RandomNumberGenerator::new();
-
-    for _ in 0..MAX_ROOMS {
-        let w = rng.range(MIN_SIZE, MAX_SIZE);
-        let h = rng.range(MIN_SIZE, MAX_SIZE);
-        let x = rng.roll_dice(1, map.width - h - 1) - 1;
-        let y = rng.roll_dice(1,map.height - w - 1) - 1;
-        let new_room = Rect::new(x, y, w, h);
-        let mut ok = true;
-        for other_room in map.rooms.iter() {
-            if new_room.intersect(other_room) { ok = false }
+    pub fn populate_blocked(&mut self) {
+        for (i,tile) in self.tiles.iter_mut().enumerate() {
+            self.blocked[i] = *tile == TileType::Wall;
         }
-        if ok {
-            map.apply_room_to_map(&new_room);
+    }
+
+    pub fn clear_content_index(&mut self) {
+        for content in self.tile_content.iter_mut() {
+            content.clear();
+        }
+    }
+
+
+    pub fn new_map_rooms_and_corridors() -> Map {
+        let mut map = Map{
+            tiles : vec![TileType::Wall; MAPCOUNT],
+            rooms : Vec::new(),
+            width : 80,
+            height : 50,
+            revealed_tiles : vec![false; MAPCOUNT],
+            visible_tiles : vec![false; MAPCOUNT],
+            blocked : vec![false; MAPCOUNT],
+            tile_content : vec![Vec::new(); MAPCOUNT]
+        };
+
+        const MAX_ROOMS : i32 = 30;
+        const MIN_SIZE : i32 = 6;
+        const MAX_SIZE : i32 = 10;
+
+        let mut rng = RandomNumberGenerator::new();
+
+        for _ in 0..MAX_ROOMS {
+            let w = rng.range(MIN_SIZE, MAX_SIZE);
+            let h = rng.range(MIN_SIZE, MAX_SIZE);
+            let x = rng.roll_dice(1, map.width - w - 1) - 1;
+            let y = rng.roll_dice(1,map.height - h - 1) - 1;
+            let new_room = Rect::new(x, y, w, h);
+            let mut ok = true;
+            for other_room in map.rooms.iter() {
+                if new_room.intersect(other_room) { ok = false }
+            }
+            if ok {
+                map.apply_room_to_map(&new_room);
 
             if !map.rooms.is_empty() {
                 let (new_x, new_y) = new_room.center();
@@ -91,18 +116,45 @@ pub fn new_map_rooms_and_corridors() -> Map {
                     map.apply_vertical_tunnel(prev_y, new_y, prev_x);
                     map.apply_horizontal_tunnel(prev_x, new_x, new_y);
                 }
-
             }
             map.rooms.push(new_room);
+            }
         }
-    }
-    map
+        map
     }
 }
 
 impl BaseMap for Map {
     fn is_opaque(&self, idx: usize) -> bool {
         self.tiles[idx] == TileType::Wall
+    }
+
+    fn get_pathing_distance(&self, idx1: usize, idx2: usize) -> f32 {
+        let w = self.width as usize;
+        let p1 = Point::new(idx1 % w, idx1 / w);
+        let p2 = Point::new(idx2 % w, idx2 / w);
+        rltk::DistanceAlg::Pythagoras.distance2d(p1, p2)
+    }
+
+    fn get_available_exits(&self, idx: usize) -> rltk::SmallVec<[(usize, f32); 10]> {
+        let mut exits = rltk::SmallVec::new();
+        let x = idx as i32 % self.width;
+        let y = idx as i32 / self.width;
+        let w = self.width as usize;
+
+        // Directions
+        if self.is_exit_valid(x-1, y) { exits.push((idx-1, 1.0)) };
+        if self.is_exit_valid(x+1, y) { exits.push((idx+1, 1.0)) };
+        if self.is_exit_valid(x, y-1) { exits.push((idx-w, 1.0)) };
+        if self.is_exit_valid(x, y+1) { exits.push((idx+w, 1.0)) };
+
+        // Diagonals
+        if self.is_exit_valid(x-1, y-1) { exits.push(((idx-w)-1, 1.45)); }
+        if self.is_exit_valid(x+1, y-1) { exits.push(((idx-w)+1, 1.45)); }
+        if self.is_exit_valid(x-1, y+1) { exits.push(((idx+w)-1, 1.45)); }
+        if self.is_exit_valid(x+1, y+1) { exits.push(((idx+w)+1, 1.45)); }
+
+        exits
     }
 }
 
@@ -136,7 +188,7 @@ pub fn draw_map(ecs: &World, ctx : &mut Rltk) {
             }
 
             x += 1;
-            if x > 79 {
+            if x > MAPWIDTH as i32-1 {
                 x = 0;
                 y += 1;
             }
