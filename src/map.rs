c8199617af6f9ac27800fac189c0e5895a0bf14a
Shaun McManus <shaun.mcmanus@gmail.com>
2021-02-21 16:48:14 -0800

    Finished Chapter 17

--- src/map.rs
+++ src/map.rs
@@ -3,6 +3,7 @@ use rltk::{ RGB, Rltk, RandomNumberGenerator, BaseMap, Algorithm2D, Point };
 use super::{Rect};
 use std::cmp::{max, min};
 use specs::prelude::*;
+use std::collections::HashSet;
 
 pub const MAPWIDTH : usize = 80;
 pub const MAPHEIGHT : usize = 43;
@@ -23,6 +24,7 @@ pub struct Map {
     pub visible_tiles : Vec<bool>,
     pub blocked : Vec<bool>,
     pub depth : i32,
+    pub bloodstains : HashSet<usize>,
 
     #[serde(skip_serializing)]
     #[serde(skip_deserializing)]
@@ -90,7 +92,8 @@ impl Map {
             visible_tiles : vec![false; MAPCOUNT],
             blocked : vec![false; MAPCOUNT],
             tile_content : vec![Vec::new(); MAPCOUNT],
-            depth: new_depth
+            depth: new_depth,
+            bloodstains: HashSet::new()
         };
 
         const MAX_ROOMS : i32 = 30;
@@ -183,6 +186,7 @@ pub fn draw_map(ecs: &World, ctx : &mut Rltk) {
             if map.revealed_tiles[idx] {
                 let glyph;
                 let mut fg;
+                let mut bg = RGB::from_f32(0., 0., 0.);
                 match tile {
                     TileType::Floor => {
                         glyph = rltk::to_cp437('.');
@@ -197,8 +201,12 @@ pub fn draw_map(ecs: &World, ctx : &mut Rltk) {
                         fg = RGB::from_f32(0., 1.0, 1.0);
                     }
                 }
-                if !map.visible_tiles[idx] { fg = fg.to_greyscale() }
-                ctx.set(x, y, fg, RGB::from_f32(0., 0., 0.), glyph);
+                if map.bloodstains.contains(&idx) { bg = RGB::from_f32(0.75, 0., 0.); }
+                if !map.visible_tiles[idx] {
+                    fg = fg.to_greyscale();
+                    bg = RGB::from_f32(0., 0., 0.);
+                }
+                ctx.set(x, y, fg, bg, glyph);
             }
 
             x += 1;
